b159955a9490c05528e5693adfc8752e1f937756
KelvinWanyama <kevmurumba@gmail.com>
2020-07-03 14:36:59 +0300

    Added a check for valid url

--- src/main/java/org/openhim/mediator/dsub/subscription/MongoSubscriptionRepository.java
+++ src/main/java/org/openhim/mediator/dsub/subscription/MongoSubscriptionRepository.java
@@ -14,6 +14,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 
+import java.net.URL;
+
 public class MongoSubscriptionRepository extends MongoSupport implements SubscriptionRepository {
 
     private static final String URL = "url";
@@ -21,6 +23,18 @@ public class MongoSubscriptionRepository extends MongoSupport implements Subscri
     private static final String TERMINATE_AT = "terminateAt";
 
     private final LoggingAdapter log;
+
+    public static boolean isUrlValid(String url) {
+
+        try {
+            new URL(url).toURI();
+            return true;
+        }
+
+        catch (Exception e) {
+            return false;
+        }
+    }
 
     public MongoSubscriptionRepository(MongoDatabase mongoDb, LoggingAdapter log) {
         super(mongoDb, "subscriptions");
@@ -35,19 +49,26 @@ public class MongoSubscriptionRepository extends MongoSupport implements Subscri
             subscription.setUuid(UUID.randomUUID().toString());
         }
 
-        Document existing = collection.find(Filters.eq(ID,
-                subscription.getUuid())).first();
-        if (existing == null) {
-            log.info("Saving subscription for: " + subscription.getUrl());
+        Document existing = collection.find(Filters.eq(ID, subscription.getUuid())).first();
+        // Check if url exists in the database and active
+        // Document existingUrl = collection.find(Filters.eq(subscription.getUrl(),
+        // URL)).first();
+        if (isUrlValid(URL)) {
+            if (existing == null) {
+                log.info("Saving subscription for: " + subscription.getUrl());
 
-            Document doc = new Document(ID, subscription.getUuid())
-                    .append(URL, subscription.getUrl())
-                    .append(TERMINATE_AT, subscription.getTerminateAt())
-                    .append(FACILITY_QUERY, subscription.getFacilityQuery());
-            collection.insertOne(doc);
+                Document doc = new Document(ID, subscription.getUuid()).append(URL, subscription.getUrl())
+                        .append(TERMINATE_AT, subscription.getTerminateAt())
+                        .append(FACILITY_QUERY, subscription.getFacilityQuery());
+                collection.insertOne(doc);
+            } else {
+                log.warning("Subscription already exists: " + subscription.getUuid());
+            }
+
         } else {
-            log.warning("Subscription already exists: " + subscription.getUuid());
+            log.warning("Invalid URL: " + URL);
         }
+
     }
 
     @Override
@@ -58,18 +79,9 @@ public class MongoSubscriptionRepository extends MongoSupport implements Subscri
 
     @Override
     public List<Subscription> findActiveSubscriptions(String facility) {
-        FindIterable<Document> result = getCollection().find(
-                Filters.and(
-                    Filters.or(
-                        Filters.eq(FACILITY_QUERY, facility),
-                        Filters.eq(FACILITY_QUERY, null)
-                    ),
-                    Filters.or(
-                            Filters.gt(TERMINATE_AT, new Date()),
-                            Filters.eq(TERMINATE_AT, null)
-                    )
-                )
-        );
+        FindIterable<Document> result = getCollection()
+                .find(Filters.and(Filters.or(Filters.eq(FACILITY_QUERY, facility), Filters.eq(FACILITY_QUERY, null)),
+                        Filters.or(Filters.gt(TERMINATE_AT, new Date()), Filters.eq(TERMINATE_AT, null))));
 
         List<Subscription> subscriptions = new ArrayList<>();
         for (Document document : result) {
@@ -81,7 +93,7 @@ public class MongoSubscriptionRepository extends MongoSupport implements Subscri
 
     private Subscription deserialize(Document document) {
         String uuid = document.getString(ID);
-        String url =document.getString(URL);
+        String url = document.getString(URL);
         Date terminateAt = document.getDate(TERMINATE_AT);
         String facilityQuery = document.getString(FACILITY_QUERY);
 
